c958f7aaf7744fca7d8c119a5290cdfa6d512eef
Kab Thangarajah <kabilanm.thangarajah@gmail.com>
2022-12-25 20:08:03 -0800

    Added rory quotes

---
+++ commands/roryquote.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('node:fs');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('rq')
+		.setDescription('Replies with a patented RORY QUOTE TM'),
+	async execute(interaction) {
+        const data = fs.readFileSync('./assets/quotes.txt');
+        if (data) {
+            const splitData = data.toString('utf8').split('\n');
+            const line = splitData.splice(Math.floor(Math.random() * splitData.length), 1)[0];
+            await interaction.reply(line);
+        }
+	},
+};
